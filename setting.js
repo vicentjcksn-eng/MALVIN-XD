5e7e025c5eae347a7029181032bfb96605d9ef79
vicentjcksn-eng <vicentjcksn@gmail.com>
2026-01-03 08:45:00 +0300

    Update setting.js

--- setting.js
+++ setting.js
@@ -1,9 +1,9 @@
 //=====🏅 ᴍᴀʟᴠɪɴ  xᴅ🔥====
 
 module.exports = {
-  BOT_NAME: 'ᴍᴀʟᴠɪɴ xᴅ', // ur bot name
-  OWNER_NAME: 'ᴍᴀʟᴠɪɴ ᴋɪɴɢ', //your name
-  OWNER_NUMBER: '263714757857', // add your phone number
+  BOT_NAME: 'VICENT xᴅ', // ur bot name
+  OWNER_NAME: 'VICENT ᴋɪɴɢ', //your name
+  OWNER_NUMBER: '255684164991', // add your phone number
   SESSION_ID: '', // add your session ID
   MODE: "public", // public or private
   TIMEZONE: 'Africa/Harare' // ur timezone
